f02f07297a8d22f259a08e372ad1b959d5976354
MBSA13102002 <73106667+MBSA13102002@users.noreply.github.com>
2022-02-28 15:41:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import requests as rm
 import json
 from flask import Flask,request
+from flask_cors import CORS
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 headers_dict = {
     "x-client-id": "78975f8bd06ac31b2981a9e1d57987",
     "x-client-secret": "3e2d92a5a03af93ddb1396af73388cf6df51f102"
@@ -19,4 +21,4 @@ def start(orderId,orderAmount):
         response = rm.post(url = "https://test.cashfree.com/api/v2/cftoken/order",json = data, headers=headers_dict)
         return json.loads(response.text)['cftoken']
 
-
+app.run()
